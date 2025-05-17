5a7825eb96ed323700b1013de89f43614be37a1e
Rahat Talukder <rahatulab@gmail.com>
2025-05-18 00:18:52 +0600

    add Title Cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
-function TitleCards() {
-  return (
-    <div className='titlecards'>TitleCards</div>
+import cards_data from '../../assets/cards/Cards_data'
+
+
+const TitleCards = () => {
+  const cardsRef = useRef();
+  const handleWheel = (e) => {
+  e.preventDefault();
+  cardsRef.current.scrollLeft += e.deltaY;
+}
+useEffect(() => {
+  cardsRef.current.addEventListener('wheel', handleWheel);
+},[]);
+return (
+    <div className='titlecards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card,index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt=""/>
+            <p>{card.name}</p>
+            </div>
+        })}
+      </div>
+    </div>
   )
 }
 
