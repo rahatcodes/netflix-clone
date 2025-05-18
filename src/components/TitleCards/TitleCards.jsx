34db5fa5ee3939ede30f14fe3deb65c506985216
Rahat Talukder <rahatulab@gmail.com>
2025-05-18 14:36:07 +0600

    add tmdb api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,25 +1,42 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = ({title,category}) => {
+
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+  
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZjMyZjhhZmFjNWI4NjNmMmNjMGQzZGM3Njc1ZGJlZSIsIm5iZiI6MTc0NzU1NTQwOC41MDcsInN1YiI6IjY4Mjk5NDUwZGU2YzQzNGQwMzhjMDEzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a8-QNuUotNb8iQbBflscaf76_0WgRYeVaNiPcnVnobk'
+  }
+};
+
+
   const handleWheel = (e) => {
   e.preventDefault();
   cardsRef.current.scrollLeft += e.deltaY;
 }
 useEffect(() => {
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
+
   cardsRef.current.addEventListener('wheel', handleWheel);
 },[]);
 return (
     <div className='titlecards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card,index)=>{
+        {apiData.map((card,index)=>{
           return <div className="card" key={index}>
-            <img src={card.image} alt=""/>
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.poster_path} alt=""/>
+            <p>{card.original_title}</p>
             </div>
         })}
       </div>
